1ef83aa19ad2c02857d552a84697178ebbab9746
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-11 22:09:16 -0300

    refactor: replace string-based feedback with compound data

--- src/main/java/com/decimo3/firsttoachieve/FirstToAchieve.java
+++ src/main/java/com/decimo3/firsttoachieve/FirstToAchieve.java
@@ -1,14 +1,21 @@
 package com.decimo3.firsttoachieve;
 
+import java.time.Instant;
+
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.advancement.Advancement;
+import net.minecraft.server.ServerAdvancementLoader;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.decimo3.firsttoachieve.PlayerAdvancementState.AdvancementRecord;
 
 public class FirstToAchieve implements ModInitializer {
 	public static final String MOD_ID = "firsttoachieve";
@@ -25,24 +32,31 @@ public class FirstToAchieve implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world from FirstToAchieve Mod!");
 		CommandRegistrationCallback.EVENT.register(
-            (dispatcher, registryAccess, environment) -> {
-				dispatcher.register(
-					CommandManager.literal("firsttoachieve")
-						.executes(context -> {
-							ServerWorld world = context.getSource().getWorld();
-							PlayerAdvancementState state = PlayerAdvancementState.get(world);
-							if (state.getAdvancements().isEmpty()) {
-								context.getSource().sendFeedback(() -> Text.literal("Nenhuma conquista registrada."), false);
-								return 1;
-							}
-							context.getSource().sendFeedback(() -> Text.literal("Conquistas registradas:"), false);
-							for (String advancementId : state.getAdvancements()) {
-								context.getSource().sendFeedback(() -> Text.literal("- " + advancementId), false);
-							}
-							return 1;
-						}
-					)
-				);
-			});
+				(dispatcher, registryAccess, environment) -> {
+					dispatcher.register(
+							CommandManager.literal("firsttoachieve")
+									.executes(context -> {
+										ServerWorld world = context.getSource().getWorld();
+										PlayerAdvancementState state = PlayerAdvancementState.get(world);
+										if (state.getAdvancements().isEmpty()) {
+											context.getSource().sendFeedback(() -> Text.literal("Nenhuma conquista registrada."), false);
+											return 1;
+										}
+										context.getSource().sendFeedback(() -> Text.literal("Conquistas registradas:"), false);
+										ServerAdvancementLoader loader = context.getSource().getServer().getAdvancementLoader();
+										for (AdvancementRecord advancement : state.getAdvancements()) {
+											Advancement advancementObj = loader.get(new Identifier(advancement.advancementId()));
+											String advancementName = (advancementObj.getDisplay() != null)
+													? advancementObj.getDisplay().getTitle().getString()
+													: advancementObj.getId().toString();
+											Instant datetime = Instant.ofEpochMilli(advancement.timestamp());
+											String message = "- " + datetime + " | " + advancement.playerName() + " | " + advancementName;
+											context.getSource().sendFeedback(() -> Text.literal(message), false);
+										}
+										return 1;
+									}
+								)
+							);
+				});
 	}
 }
